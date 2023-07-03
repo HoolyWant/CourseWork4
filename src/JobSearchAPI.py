8b703897840c7890f2e44add1af846a7b6f59484
hoolywant <sir.saltickow@yandex.ru>
2023-07-03 21:17:54 +0300

    connect to HH API

--- src/JobSearchAPI.py
+++ src/JobSearchAPI.py
@@ -1,4 +1,7 @@
 from abc import ABC, abstractmethod
+from pprint import pprint
+from requests import get
+import json
 
 
 class JobSearchAPI(ABC):
@@ -9,14 +12,16 @@ class JobSearchAPI(ABC):
 
 
 class HeadHunterAPI(JobSearchAPI):
-
     def get_vacancies(self, specialty):
-        pass
+        response = json.loads(get(f'https://api.hh.ru/vacancies?text='
+                                  f'{specialty.lower()}&area=113').text)
+        vacansies = json.dumps(response['items'], indent=4)
+        with open('hh_vacansies.json', 'w', encoding='utf-8') as hh_vacansies_json:
+            hh_vacansies_json.write(vacansies)
+
 
 
 class SuperJobAPI(JobSearchAPI):
 
     def get_vacancies(self, specialty):
         pass
-
-
