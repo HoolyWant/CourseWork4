4861886b035996175bb026674311f08e0a5ca5cb
hoolywant <sir.saltickow@yandex.ru>
2023-07-06 20:14:00 +0300

    little fix for utils

--- src/utils.py
+++ src/utils.py
@@ -3,8 +3,9 @@ def get_top(list_):
                       f'которое вы хотели бы видеть из '
                       f'{len(list_)}:\n'))
     top_list = list_[:top_n]
-    print(f'\nСписок вакансий по вашему запросу в количестве {top_n}:\n')
+    print(f'\nТоп вакансий по вашему запросу в количестве {top_n}:\n')
     for item in top_list:
         for key, value in item.items():
             print(value)
         print('\n')
+    return list_[:top_n]
