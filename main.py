13d52b5ac6e092aa8f68acb4d86973bd69a544b2
hoolywant <sir.saltickow@yandex.ru>
2023-07-06 16:26:40 +0300

    working on the user interaction

--- main.py
+++ main.py
@@ -1,35 +1,32 @@
-# Создание экземпляра класса для работы с API сайтов с вакансиями
-hh_api = HeadHunterAPI()
-superjob_api = SuperJobAPI()
+from pprint import pprint
 
-# Получение вакансий с разных платформ
-hh_vacancies = hh_api.get_vacancies("Python")
-superjob_vacancies = superjob_api.get_vacancies("Python")
+from src.JobSearchAPI import HeadHunterAPI, SuperJobAPI
 
-# Создание экземпляра класса для работы с вакансиями
-vacancy = Vacancy("Python Developer", "<https://hh.ru/vacancy/123456>", "100 000-150 000 руб.")
 
-# Сохранение информации о вакансиях в файл
-json_saver = JSONSaver()
-json_saver.add_vacancy(vacancy)
-json_saver.get_vacancies_by_salary("100 000-150 000 руб.")
-json_saver.delete_vacancy(vacancy)
-
-# Функция для взаимодействия с пользователем
 def user_interaction():
-    platforms = ["HeadHunter", "SuperJob"]
-    search_query = input("Введите поисковый запрос: ")
-    top_n = int(input("Введите количество вакансий для вывода в топ N: "))
-    filter_words = input("Введите ключевые слова для фильтрации вакансий: ").split()
-    filtered_vacancies = filter_vacancies(hh_vacancies, superjob_vacancies, filter_words)
+    user_search_request = str(input('Введите поисковой запрос: '))
+    hh = HeadHunterAPI()
+    supjob = SuperJobAPI()
+    attributes = [hh, supjob]
+    user_choice = int(input('\nВведите номер платформы,\n'
+                             'с которой вы хотите работать:\n'
+                             '1.HH\n'
+                             '2.SuperJob\n'))
+    platform = attributes[user_choice-1]
+    platform_api = platform.get_vacancies(user_search_request)
+    platform_vacancies = platform.clear_vacancies_list(platform_api)
+    pprint(platform_vacancies)
+    # top_n = int(input('\nВведите количество вакансий,\n'
+    #                   f'которое вы хотели бы видеть из '
+    #                   f'{len(platform_vacancies)}:\n'))
+    # top_list = platform_vacancies[:top_n-1]
+    # print(f'\nСписок вакансий по вашему запросу в количестве {top_n}')
+    # for item in top_list:
+    #     for key, value in item.items():
+    #         print(value)
+    #     print('\n')
 
-    if not filtered_vacancies:
-        print("Нет вакансий, соответствующих заданным критериям.")
-        return
 
-    sorted_vacancies = sort_vacancies(filtered_vacancies)
-    top_vacancies = get_top_vacancies(sorted_vacancies, top_n)
-    print_vacancies(top_vacancies)
 
 
 if __name__ == "__main__":
