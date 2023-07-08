71baa0d3779f74a8ad683894738b41ef8ca9d97e
hoolywant <sir.saltickow@yandex.ru>
2023-07-08 23:33:50 +0300

    main file is ready

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-from pprint import pprint
 from src.utils import get_top
 from src.JobSearchAPI import HeadHunterAPI, SuperJobAPI
 from src.vacancy import Vacancy
 from src.Saver import JSONSaver
 
 
-def user_interaction():
+def user_interaction(hh_api, superjob_api):
+    """
+    Функция для работы пользователя с платформами:
+    HH и SuperJob
+    """
     user_search_request = str(input('Введите поисковой запрос: '))
-    hh_api = HeadHunterAPI()
-    superjob_api = SuperJobAPI()
     user_choice = int(input('\nВведите номер платформы,\n'
                              'с которой вы хотите работать:\n'
                              '1.HH\n'
@@ -27,19 +28,65 @@ def user_interaction():
         print('Такого номера не существует(')
 
 
-
 if __name__ == "__main__":
-    # user_interaction()
+    # Создание экземпляров для работы с апи
+    hh_api = HeadHunterAPI()
+    superjob_api = SuperJobAPI()
+
+    # Вызов функции для пользователя
+    user_interaction(hh_api, superjob_api)
+
+    # Объявление экземпляров класса для работы с вакансиями
     vacancy1 = Vacancy('Диспетчер чатов, удаленно', 'https://hh.ru/vacancy/83028213',
                        '40000', 'Россия')
     vacancy2 = Vacancy('Начинающий программист (стажер)', 'https://hh.ru/vacancy/82868479', '30000',
                        'Новосибирск')
+
+    # Тесты сравнения вакансий по зарплате
     assert vacancy1 > vacancy2
+    assert vacancy2 < vacancy1
+
+    # Получение словарей из атрибутов вакансии для дальнейшей работы
     vacancy_dict1 = vacancy1.get_dict()
     vacancy_dict2 = vacancy2.get_dict()
+
+    # Вызов экземпляра класса для работы с файлами JSON
     json = JSONSaver()
-    # json.clear_file()
+
+    # Очищает словарь и готовит к работе,
+    # вызывается вручную для корректной записи
+    json.clear_file()
+
+    # Добавление и удаление вакансий по одной в
+    # файле custom_vacancies_list.json
     json.add_vacancy(vacancy_dict2)
     json.add_vacancy(vacancy_dict1)
     json.add_vacancy(vacancy_dict1)
     json.delete_vacancy(vacancy_dict2)
+    json.delete_vacancy(vacancy_dict1)
+    json.delete_vacancy(vacancy_dict1)
+    # Конечный результат в файле: []
+
+    # Пример списка с вакансиями
+    vacancies_list = [
+                      {
+                        'name': 'Начинающий программист (стажер)', 'url': 'https://hh.ru/vacancy/82868479',
+                        'area': 'Новосибирск', 'salary': '50000'
+                      },
+                      {
+                        'name': 'Диспетчер чатов, удаленно', 'url': 'https://hh.ru/vacancy/83028213',
+                        'area': 'Россия', 'salary': '40000'
+                      }
+    ]
+
+    # Метод заносит готовый список вакансий в файл vacancies_list.json
+    json.save_vacancies_list(vacancies_list)
+
+    # Файл, из которого мы хотим выбрать вакансию по критерию
+    file = 'vacancies_list.json'
+
+    # Критерий по которому фильтруются вакансии
+    criterion = 'Новосибирск'
+
+    # Вызов метода  по поиску вакансий в файле
+    vacancies_from_file = json.get_vacancy(file, criterion)
